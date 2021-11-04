ab92a57135621060f107cdf5007160b53575a36f
martsilva <martin.silva@mercadolibre.com>
2021-11-03 21:00:14 -0300

    Navigation to and from main-terms fragments

--- app/src/main/java/com/example/integradorandroid/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/integradorandroid/ui/main/MainFragment.kt
@@ -6,33 +6,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.integradorandroid.R
 import com.example.integradorandroid.databinding.FragmentTermsBinding
 import com.example.integradorandroid.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
 
-
-    companion object {
-        fun newInstance() = MainFragment()
-    }
-
     private var _binding: MainFragmentBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         _binding = MainFragmentBinding.inflate(inflater, container, false)
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        binding.tvTerms.setOnClickListener {
+            val action = MainFragmentDirections.actionMainFragmentToTermsFragment()
+            findNavController().navigate(action)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
 }
--- app/src/main/java/com/example/integradorandroid/ui/main/TermsFragment.kt
+++ app/src/main/java/com/example/integradorandroid/ui/main/TermsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.integradorandroid.R
 import com.example.integradorandroid.databinding.FragmentTermsBinding
 
@@ -19,12 +20,27 @@ class TermsFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentTermsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnX.setOnClickListener {
+            val action = TermsFragmentDirections.actionTermsFragmentToMainFragment()
+            findNavController().navigate(action)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 
