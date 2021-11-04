1aad21deb72ca6d51c967334f35500bf5543be95
pazapata <paula.zapata@mercadolibre.com.co>
2021-11-04 12:30:09 -0500

    creacion de diseño para categorias ajuste de strings y colores quemados en los layouts creacion de toolbar para la app

--- app/src/main/java/com/example/integradorandroid/ui/main/Category.kt
+++ app/src/main/java/com/example/integradorandroid/ui/main/Category.kt
@@ -2,6 +2,7 @@ package com.example.integradorandroid.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.integradorandroid.R
 
 class Category : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
